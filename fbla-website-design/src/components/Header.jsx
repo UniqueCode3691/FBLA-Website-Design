86994f5302512596f551a6b433b3d5c2a44c6c05
Sam Mergu <samanyu3691@gmail.com>
2025-01-26 17:31:01 -0500

    Added Color Palette and properly set up Tailwind

---
+++ fbla-website-design/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="flex">
+            <div className="bg-maplebrown"></div>
+            <div className="bg-maplebrown"></div>
+            <div className="bg-maplebrown"></div>
+            <div className="bg-maplebrown"></div>
+            <div className="bg-maplebrown"></div>
+        </header>
+    )
+}
